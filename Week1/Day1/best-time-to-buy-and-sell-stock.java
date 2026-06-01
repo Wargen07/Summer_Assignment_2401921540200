eddcce98eea227d35ec63866894de06c0e58820a
Rohan Pandey <rohanpandeyy07@gmail.com>
2026-06-01 23:51:51 +0530

    Add files via upload

---
+++ Week1/Day1/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, buy = prices[0] ;
+        for(int i = 1 ; i<prices.length ; i++){
+            if(prices[i] > buy){
+                profit = Math.max(profit,prices[i] - buy);
+            }
+            buy = Math.min(buy,prices[i]);
+        }
+        return profit ;
+    }
+}
